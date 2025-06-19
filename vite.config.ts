766696c30dbb62d4cd70bcda6c4b7844cc617798
Ajay2760 <zeusgamer0157@gmail.com>
2025-06-19 11:01:09 +0530

    Resolved merge conflicts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,8 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
-  base: "/Carbon-footprint-calculator/",
   plugins: [
     react(),
-    runtimeErrorOverlay(),
     ...(process.env.NODE_ENV !== "production" &&
     process.env.REPL_ID !== undefined
       ? [
